ac7e41b6ce1dbbfc20b02e5def8d3505ca33713f
bgsilham <ilhambagas92@gmail.com>
2020-06-21 15:19:46 +0700

    styling

---
+++ src/pages/Home.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Row, Col, Input, Button } from 'reactstrap'
+
+export default class Home extends Component {
+  render() {
+    return(
+      <>
+        <center>
+          <div className='calculator'>
+            <Row>
+              <Col>
+                <Input className='input' />
+              </Col>
+            </Row>
+            <Row className='row-btn'>
+              <Col>
+                <Button className='btn btn-dark'>AC</Button>
+                <Button className='btn btn-dark'>+/-</Button>
+                <Button className='btn btn-dark'>%</Button>
+                <Button className='btn btn-orange'>/</Button>
+              </Col>
+              <Col className='col-btn'>
+                <Button className='btn btn-light'>7</Button>
+                <Button className='btn btn-light'>8</Button>
+                <Button className='btn btn-light'>9</Button>
+                <Button className='btn btn-orange'>X</Button>
+              </Col>
+              <Col className='col-btn'>
+                <Button className='btn btn-light'>4</Button>
+                <Button className='btn btn-light'>5</Button>
+                <Button className='btn btn-light'>6</Button>
+                <Button className='btn btn-orange'>-</Button>
+              </Col>
+              <Col className='col-btn'>
+                <Button className='btn btn-light'>1</Button>
+                <Button className='btn btn-light'>2</Button>
+                <Button className='btn btn-light'>3</Button>
+                <Button className='btn btn-orange'>+</Button>
+              </Col>
+              <Col className='col-btn'>
+                <Button className='btn-zero'>0</Button>
+                <Button className='btn btn-light'>.</Button>
+                <Button className='btn btn-orange'>=</Button>
+              </Col>
+            </Row>
+          </div>     
+        </center>
+      </>
+    )
+  }
+}
